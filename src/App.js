0944fab64bff625721373154b42f13575be93b38
Peram Kharel <peram1769@gmail.com>
2022-01-09 20:07:55 +0800

    merged with zewai and working

--- src/App.js
+++ src/App.js
@@ -1,59 +1,9 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import GanttChart from './Components/GannttChart/GanttChart';
-import Upcoming from './Components/Upcoming/Upcoming';
+import GanttChart from './components/GannttChart/GanttChart';
+import Upcoming from './components/Upcoming/Upcoming';
 
 function App() {
 
-  let flowers = [
-    {
-        title: "Chinese New Year Market",
-        startPlanting: "May 2022",
-        readyBy: "Feb 2023",
-        image: "https://images.unsplash.com/photo-1516477266610-9e4c763da721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-        description: "some short desription ",
-        label: "Event"
-    },
-    {
-        title: "Halloween pumpkin picking",
-        startPlanting: "Jan 2022",
-        readyBy: "Oct 2022",
-        image: "https://images.unsplash.com/photo-1508995476428-43d70c3d0042?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-        description: "some short desription ",
-        label: "Event"
-    },
-    {
-        title: "Strawberry picking",
-        startPlanting: "Nov 2022",
-        readyBy: "April 2023",
-        image: "https://images.unsplash.com/photo-1592300254886-5281e714c86b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
-        description: "some short desription ",
-        label: "Workshop"
-    },
-    {
-        title: "Herb sowing",
-        startPlanting: "May 2022",
-        readyBy: "Sep 2023",
-        image: "https://images.unsplash.com/photo-1472141341085-dab5ea0df9a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
-        description: "some short desription ",
-        label: "Workshop"
-    },
-    {
-        title: "Christmas herb picking",
-        startPlanting: "Feb 2022",
-        readyBy: "Dec 2022",
-        image: "https://images.unsplash.com/photo-1594313016519-640ed47407ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
-        description: "some short desription ",
-        label: "Workshop"
-    },
-    {
-        title: "Valentine's day market",
-        startPlanting: "Nov 2022",
-        readyBy: "Feb 2023",
-        image: "https://images.unsplash.com/photo-1612328289561-0edd72aefc43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
-        description: "some short desription ",
-        label: "Event"
-    }
-]
 
 
 
--- src/Components/Upcoming/Packages/PackageCard/PackageCard.js
+++ src/Components/Upcoming/Packages/PackageCard/PackageCard.js
@@ -3,22 +3,24 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock, faTimes } from '@fortawesome/free-solid-svg-icons';
+import styles from './Upcoming.module.css';
+
 
 export default function PackageCard(props) {
     if(props.label == 'Event') {
         return (
             <>
-                <Card className='cardBody' style={{ width: '18rem' }}>
-                    <Card.Img  className='cardImg' variant="top" src={props.image} />
-                    <h3 className='label'>{props.label}</h3>
+                <Card className={styles.cardBody} style={{ width: '18rem' }}>
+                    <Card.Img  className={styles.cardImg} variant="top" src={props.image} />
+                    <h3 className={styles.label}>{props.label}</h3>
                     <Card.Body>
                         <Card.Title className="text-center"><h3>{props.title}</h3></Card.Title>
                         <Card.Text>
-                            <p className='cardDetails'><FontAwesomeIcon icon={faTimes} />Start Planting: <span className='detailDate'>{props.startPlanting}</span></p>
-                            <p className='cardDetails'><FontAwesomeIcon icon={faClock} />Ready By: <span className='detailDate'>{props.readyBy}</span></p>
+                            <p className={styles.cardDetails}><FontAwesomeIcon icon={faTimes} />Start Planting: <span className='detailDate'>{props.startPlanting}</span></p>
+                            <p className={styles.cardDetails}><FontAwesomeIcon icon={faClock} />Ready By: <span className='detailDate'>{props.readyBy}</span></p>
                         </Card.Text>
                         <div className='d-flex justify-content-center'>
-                            <Button className='cardButton' variant="primary">View Package</Button>
+                            <Button className={styles.cardButton} variant="primary">View Package</Button>
                         </div>
                     </Card.Body>
                 </Card>
@@ -27,17 +29,17 @@ export default function PackageCard(props) {
     }
     return (
             <>
-                <Card className='cardBody' style={{ width: '18rem' }}>
-                    <Card.Img  className='cardImg' variant="top" src={props.image} />
-                    <h3 className='labelWorkshop'>{props.label}</h3>
+                <Card className={styles.cardBody} style={{ width: '18rem' }}>
+                    <Card.Img  className={styles.cardImg} variant="top" src={props.image} />
+                    <h3 className={styles.labelWorkshop}>{props.label}</h3>
                     <Card.Body>
                         <Card.Title className="text-center"><h3>{props.title}</h3></Card.Title>
                         <Card.Text>
-                            <p className='cardDetails'><FontAwesomeIcon icon={faTimes} />Start Planting: <span className='detailDate'>{props.startPlanting}</span></p>
-                            <p className='cardDetails'><FontAwesomeIcon icon={faClock} />Ready By: <span className='detailDate'>{props.readyBy}</span></p>    
+                            <p className={styles.cardDetails}><FontAwesomeIcon icon={faTimes} />Start Planting: <span className='detailDate'>{props.startPlanting}</span></p>
+                            <p className={styles.cardDetails}><FontAwesomeIcon icon={faClock} />Ready By: <span className='detailDate'>{props.readyBy}</span></p>    
                         </Card.Text>
                         <div className='d-flex justify-content-center'>
-                            <Button className='cardButton' variant="primary">View Package</Button>
+                            <Button className={styles.cardButton} variant="primary">View Package</Button>
                         </div>
                     </Card.Body>
                 </Card>
