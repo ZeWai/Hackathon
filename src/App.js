36532602fe321d535b195f2c5b1a8ac5cbc7eb82
Ivan Cheng <ivanchengzhongyan@gmail.com>
2022-01-09 21:55:08 +0800

    try to merge this bitch

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ function App() {
         readyBy: "13/05/2021",
         image: "https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=568adfe5194c08f3432390dd7f730d74",
         description: "some short desription ",
+        plants: {
+          name: "wood",
+          img: "h"
+        }
        
     },
     {
