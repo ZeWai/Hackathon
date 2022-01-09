654cdf3fe3012b83b6aab84d14c155c1496924d0
Ivan Cheng <ivanchengzhongyan@gmail.com>
2022-01-09 19:19:36 +0800

    finished modal

--- src/Components/Upcoming/Packages/packagemodal/PackageModal.js
+++ src/Components/Upcoming/Packages/packagemodal/PackageModal.js
@@ -19,23 +19,36 @@ const PackageModal = (props) => {
           <Container class="container-fluid">
             <Row>
               <Col md={7}>
-                
-                  <div class="modal1">
-                    <Modal.Title class="text1">
-                      <p>Step 1: Pick your choice</p>
-                    </Modal.Title>
-                    <p class ="text2">{props.title}</p>
-                    <p class="text3">
-                      Pick the flowers you would like to plan for your event
-                    </p>
+                <div class="modal1">
+                  <Modal.Title class="text1">
+                    <p>Step 1: Pick your choice</p>
+                  </Modal.Title>
+                  <p class="text2">{props.title}</p>
+                  <p class="text3">
+                    Pick the flowers you would like to plan for your event
+                  </p>
+                </div>
+                <div class="container vertical-scrollable">
+                  <div class="row text-center">
+                    {props.plants.map((e) => {
+                      return (
+                        <div className="plants">
+                          <Col md={9}>
+                            <Col md={3}>
+                              <Button variant="primary">Add</Button>
+                            </Col>
+                            <Col md={9}>
+                              <p>{e.name}</p>
+                            </Col>
+                          </Col>
+                          <Col md={3}>
+                            <Image src={e.image} fluid />
+                          </Col>
+                        </div>
+                      );
+                    })}
                   </div>
-                  <div class="container vertical-scrollable">
-                    <div class="row text-center">
-                      
-                    </div>
-                  </div>
-
-                
+                </div>
               </Col>
               <Col md={5}>
                 <Image src={props.image} fluid />
