46412a98fb0f95cb8fefd611f0d0abc20c895fc5
Peram Kharel <peram1769@gmail.com>
2022-01-09 19:07:13 +0800

    some styling in package

--- src/App.js
+++ src/App.js
@@ -7,46 +7,52 @@ function App() {
 
   let flowers = [
     {
-        title: "Chinese New Year",
-        startPlanting: "12/12/2020",
-        readyBy: "13/05/2021",
-        image: "https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=568adfe5194c08f3432390dd7f730d74",
-        description: "some short desription "
+        title: "Chinese New Year Market",
+        startPlanting: "May 2022",
+        readyBy: "Feb 2023",
+        image: "https://images.unsplash.com/photo-1516477266610-9e4c763da721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+        description: "some short desription ",
+        label: "Event"
     },
     {
-        title: "Chinese New Year",
-        startPlanting: "12/12/2020",
-        readyBy: "13/05/2021",
-        image: "https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=568adfe5194c08f3432390dd7f730d74",
-        description: "some short desription "
+        title: "Halloween pumpkin picking",
+        startPlanting: "Jan 2022",
+        readyBy: "Oct 2022",
+        image: "https://images.unsplash.com/photo-1508995476428-43d70c3d0042?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+        description: "some short desription ",
+        label: "Event"
     },
     {
-        title: "Chinese New Year",
-        startPlanting: "12/12/2020",
-        readyBy: "13/05/2021",
-        image: "https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=568adfe5194c08f3432390dd7f730d74",
-        description: "some short desription "
+        title: "Strawberry picking",
+        startPlanting: "Nov 2022",
+        readyBy: "April 2023",
+        image: "https://images.unsplash.com/photo-1592300254886-5281e714c86b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
+        description: "some short desription ",
+        label: "Workshop"
     },
     {
-        title: "Chinese New Year",
-        startPlanting: "12/12/2020",
-        readyBy: "13/05/2021",
-        image: "https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=568adfe5194c08f3432390dd7f730d74",
-        description: "some short desription "
+        title: "Herb sowing",
+        startPlanting: "May 2022",
+        readyBy: "Sep 2023",
+        image: "https://images.unsplash.com/photo-1472141341085-dab5ea0df9a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
+        description: "some short desription ",
+        label: "Workshop"
     },
     {
-        title: "Chinese New Year",
-        startPlanting: "12/12/2020",
-        readyBy: "13/05/2021",
-        image: "https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=568adfe5194c08f3432390dd7f730d74",
-        description: "some short desription "
+        title: "Christmas herb picking",
+        startPlanting: "Feb 2022",
+        readyBy: "Dec 2022",
+        image: "https://images.unsplash.com/photo-1594313016519-640ed47407ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+        description: "some short desription ",
+        label: "Workshop"
     },
     {
-        title: "Chinese New Year",
-        startPlanting: "12/12/2020",
-        readyBy: "13/05/2021",
-        image: "https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=568adfe5194c08f3432390dd7f730d74",
-        description: "some short desription "
+        title: "Valentine's day market",
+        startPlanting: "Nov 2022",
+        readyBy: "Feb 2023",
+        image: "https://images.unsplash.com/photo-1612328289561-0edd72aefc43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+        description: "some short desription ",
+        label: "Event"
     }
 ]
 
--- src/components/packageCard/packageCard.js
+++ src/components/packageCard/packageCard.js
@@ -5,19 +5,44 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 export default function PackageCard(props) {
+    if(props.label == 'Event') {
+        return (
+            <>
+                <Card className='cardBody' style={{ width: '18rem' }}>
+                    <Card.Img  className='cardImg' variant="top" src={props.image} />
+                    <h3 className='label'>{props.label}</h3>
+                    <Card.Body>
+                        <Card.Title className="text-center"><h3>{props.title}</h3></Card.Title>
+                        <Card.Text>
+                            <p className='cardDetails'><FontAwesomeIcon icon={faTimes} />Start Planting: <span className='detailDate'>{props.startPlanting}</span></p>
+                            <p className='cardDetails'><FontAwesomeIcon icon={faClock} />Ready By: <span className='detailDate'>{props.readyBy}</span></p>
+                        </Card.Text>
+                        <div className='d-flex justify-content-center'>
+                            <Button className='cardButton' variant="primary">View Package</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </>
+        );
+    }
     return (
-        <>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={props.image} />
-                <Card.Body className="text-center">
-                    <Card.Title><h3>{props.title}</h3></Card.Title>
-                    <Card.Text>
-                        <p className=''><FontAwesomeIcon icon={faTimes} />Start Planting: {props.startPlanting}</p>
-                        <p className=''><FontAwesomeIcon icon={faClock} />Ready By: {props.readyBy}</p>
-                    </Card.Text>
-                    <Button variant="primary">View Package</Button>
-                </Card.Body>
-            </Card>
-        </>
-    );
+            <>
+                <Card className='cardBody' style={{ width: '18rem' }}>
+                    <Card.Img  className='cardImg' variant="top" src={props.image} />
+                    <h3 className='labelWorkshop'>{props.label}</h3>
+                    <Card.Body>
+                        <Card.Title className="text-center"><h3>{props.title}</h3></Card.Title>
+                        <Card.Text>
+                            <p className='cardDetails'><FontAwesomeIcon icon={faTimes} />Start Planting: <span className='detailDate'>{props.startPlanting}</span></p>
+                            <p className='cardDetails'><FontAwesomeIcon icon={faClock} />Ready By: <span className='detailDate'>{props.readyBy}</span></p>    
+                        </Card.Text>
+                        <div className='d-flex justify-content-center'>
+                            <Button className='cardButton' variant="primary">View Package</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </>
+        );
 }
+// <FontAwesomeIcon icon={faTimes} />
+// <FontAwesomeIcon icon={faClock} />
